732f9d26d4ce0ee1530390133bba305d1995c88f
Joaquin Irusta <joaquinirustaa@gmail.com>
2025-08-18 20:12:04 -0300

    Comienza el desarrollo de la app

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+// funcionamiento del boton para agregar amigos
+function agregarAmigo(){
+    let nombreUsuario = document.getElementById(`amigo`).value;
+    if (document.getElementById(`amigo`).value == ``){
+        alert("Por favor, inserte un nombre.");
+    } else{
+        listaAmigos.push(nombreUsuario);
+        limpiarCaja();
+        listaVisual();
+    }
+}
+
+//funcion para la lista de amigos visual de la web
+function listaVisual() {
+   let lista = document.getElementById("listaAmigos");
+   lista.innerHTML = ``;
+   lista.innerHTML = listaAmigos;// modificar, usar bucle for
+}
+
+// limpiar el espacio de input
+function limpiarCaja(){
+    document.querySelector(`#amigo`).value = ``;
+}
