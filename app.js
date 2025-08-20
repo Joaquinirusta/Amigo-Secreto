46b876c34531dc79f67c01511cbde5b21ccf655a
Joaquin Irusta <joaquinirustaa@gmail.com>
2025-08-20 19:51:57 -0300

    Finaliza el desarrollo de la app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let amigosElegidos = [];
 // funcionamiento del boton para agregar amigos
 function agregarAmigo(){
     let nombreUsuario = document.getElementById(`amigo`).value;
-    if (document.getElementById(`amigo`).value == ``){
+    if (nombreUsuario == ``){
         alert("Por favor, inserte un nombre.");
     } else if(amigosElegidos.includes(nombreUsuario)){
         alert("El nombre ingresado ya esta en el sorteo");
@@ -23,10 +23,24 @@ function listaVisual() {
         nombre += `<li>${amigosElegidos[i]}</li>`;
     }
         lista.innerHTML = nombre;
-        console.log(amigosElegidos);
 }
 
-// limpiar el espacio de input
+//funcion del boton para realizar el sorteo
+function sortearAmigo() {
+    let nombreElegido = '';
+    let resultado = document.getElementById("resultado");
+    if (amigosElegidos.length <= 1) {
+        alert("Debe ingresar al menos 2 nombres para realizar el sorteo");
+    } else {
+        let num = Math.floor(Math.random()*amigosElegidos.length);
+        nombreElegido = amigosElegidos[num];
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = `Su amigo secreto es: ${nombreElegido}`;
+       
+        }
+} 
+     
+      // limpiar el espacio de input
 function limpiarCaja(){
     document.querySelector(`#amigo`).value = ``;
 }
